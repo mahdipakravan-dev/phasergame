856786c17c82490efe3769dd872f4c5c970c790e
mahdipakravan-dev <mahdipakravan79@gmail.com>
2023-04-19 23:23:15 +0000

    ci

---
+++ ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git fetch
+if git diff --quiet HEAD FETCH_HEAD; then
+  echo "No changes in the remote master branch."
+else
+  # Pull the latest changes from the remote master branch
+  git pull origin master
+
+  # Install the dependencies
+  yarn
+
+  # Build
+  yarn build
+
+  mv dist html
+  mv ./html /var/www/game.mahdipakravan.ir/
+fi
